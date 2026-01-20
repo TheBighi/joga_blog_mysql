48f248955d44e149ab07b7e4a4223d317a9a7f35
TheBighi <thebighix@gmail.com>
2026-01-20 09:30:43 +0200

    displayed all articles

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const Express = require("express")
 const path = require('path')
 const mysql = require('mysql2')
 const bodyParser = require('body-parser')
+const hbs = require('express-handlebars')
 
 const app = Express()
 
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "hbs")
+app.engine("hbs", hbs.engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts/"
+}))
+
+app.use(Express.static("public"))
+
 app.use(bodyParser.urlencoded({extended: true}))
+
+
+
 
 const con = mysql.createConnection({
     host: 'localhost',
@@ -17,6 +31,17 @@ const con = mysql.createConnection({
 con.connect((err) =>{
     if (err) throw err
     console.log('connected to mysql db')
+})
+
+app.get('/', (req, res) => {
+    let query = 'SELECT * FROM article'
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err
+        articles = result
+        console.log(articles)
+        res.render('index', { articles:articles })
+    })
 })
 
 app.listen(3001, () =>{
